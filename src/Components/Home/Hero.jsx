ff794ab6c7e58e8b4438dde2692067a1d5b7867e
Anuran12 <anuranarts@gmail.com>
2024-03-29 17:09:59 +0530

    update hero section

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -11,35 +11,96 @@ export default function Hero() {
 
         <>
             <section className='lg:h-[650px] w-full work-sans relative bg-white' id='home'>
-                <div className='overflow-hidden w-full'>
-                    {/* <div className='linear-shade absolute -top-[50px] md:-top-[400px] md:left-1/2 -translate-x-1/2'></div>
-                    <div className='lg:block hidden linear-shade absolute md:-top-[700px] md:left-1/2 left-[150px] -top-[200px] -translate-x-[10%]'></div> */}
-                    <img src={bgDrop} alt="Nothing" className='absolute left-0 top-0 md:left-[58%] -translate-x-1/2 md:-top-28 md:w-auto overflow-hidden' />
-                </div>
-
-                <div className={`${styles.maxWidth} text-white h-full flex lg:items-center justify-center z-[1] relative lg:mt-0 mt-32 md:mt-40 lg:mb-0 md:mb-32 sm:mb-16`}>
-                    <div className='lg:px-0 px-6'>
-                        <h1 className={`${styles.heroHeadText} font-bold text-center w-full max-w-[1120px] capitalize text-black`}>
-                            We Craft Digital Experiences
-                            That <span className='text-light-green'>Resonate</span> and <span className='text-neal-blue'>Convert</span>
-                        </h1>
-                        <p className={`text-base font-light md:text-lg md:font-normal w-full pt-6 max-w-[680px] text-center m-auto text-black`}>
-                            Our mission is to empower businesses by enhancing their online presence with innovative, custom-made digital solutions.
-                        </p>
-                        <div className='flex md:flex-row flex-col gap-y-4 justify-center items-center gap-x-4 mt-8 md:-mb-12 md:mt-12 overflow-hidden'>
-                            <Link to="tel:(323) 595-3041" className='w-full md:w-auto'>
-                                <button className={`${btnStyles} text-white bg-custom-blue hover:bg-[#0264bf]`}>
-                                    <span className='relative z-10'>Call or Text (323) 595-3041</span>
-                                </button>
-                            </Link>
-                            <a href="https://calendly.com/goastrodigital/exploratory-call" target='_blank' className='w-full md:w-auto'>
-                                <button className={`${btnStyles} bg-transparent border border-black before:absolute before:h-0 before:w-0 before:rounded-full hover:border-[#0270bf] text-black hover:text-[#0270bf]`}>
-                                    <span className='relative z-10'>Schedule a 15 Minute Call</span>
-                                </button>
-                            </a>
+            <div className='min-h-screen overflow-hidden relative'>
+        <div class="container__glow2"></div>
+        <div className='hero flex flex-row h-[100vh] w-full mt-10'>
+            <div className='left h-full w-[60%] flex justify-center items-center pl-[120px]'>
+                <div className='flex flex-col gap-20 -mt-16'>
+                    <h1 className={`text-[55px] text-black font-bold`}>We Craft Digital Experiences<br/> That <span className='bg-gradient-to-br from-[#00FFFF] to-[#000061] bg-clip-text text-transparent'>Resonate</span> and <span className='bg-gradient-to-br from-[#00FFFF] to-[#000061] bg-clip-text text-transparent'>Convert</span></h1>
+                    <p className={`text-black text-[22px]`}>
+                    Our mission is to empower businesses by enhancing their<br/> online presence with innovative, custom-made digital solutions.
+                    </p>
+                    <div className='flex flex-row gap-5 items-center'>
+                        <div className='p-[1.5px] px-16 py-2 rounded-full w-fit bg-gradient-to-br from-[#00FFFF] to-[#000061]'>
+                            <h1 className={`text-white font-medium text-[18px] leading-5 text-center`}>Text Us<br/> &#40;323&#41; 595-3041</h1>
+                        </div>
+                        <h1 className={`text-black font-medium text-[22px]`}>OR</h1>
+                        <div className='p-[1.5px] rounded-full w-fit bg-gradient-to-br from-[#00FFFF] to-[#000061]'>
+                            <div className='px-9 py-3 rounded-full w-fit bg-white'>
+                                <h1 className={` font-medium text-[18px] bg-gradient-to-br from-[#00FFFF] to-[#000061] bg-clip-text text-transparent`}>Schedule a 15 Minute Call</h1>
+                            </div>
                         </div>
                     </div>
                 </div>
+
+            </div>
+            <div className='right h-full w-[40%] relative overflow-hidden -top-10 pt-6'>
+
+                <ul className='orbit-wrap z-10 absolute w-[29em] aspect-square rounded-full border-solid border-2 border-[#3FBAFF] -right-48 p-8'>
+                    <li>
+                    <ul class="ring-0">
+                        <li><i class="orbit-icon"><svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M19.2837 15.042L13.3137 2.23898C12.9744 1.51135 12.3599 0.948314 11.6055 0.673741C10.851 0.399169 10.0184 0.43555 9.29074 0.774881C8.56311 1.11421 8.00007 1.7287 7.7255 2.48315C7.45093 3.23761 7.48731 4.07024 7.82664 4.79788L13.7971 17.6004C14.1363 18.3281 14.7507 18.8911 15.5052 19.1657C16.2596 19.4404 17.0922 19.4041 17.8198 19.0648C18.5474 18.7255 19.1105 18.1111 19.3851 17.3567C19.6597 16.6023 19.6229 15.7697 19.2837 15.042Z" stroke="white"/>
+                            <path d="M10.2427 11.1987L7.25695 17.6C7.08894 17.9603 6.85161 18.284 6.55851 18.5526C6.2654 18.8211 5.92227 19.0293 5.54869 19.1653C5.17512 19.3013 4.77842 19.3623 4.38126 19.3449C3.98409 19.3276 3.59423 19.2321 3.23395 19.0641C2.50642 18.7248 1.94346 18.1104 1.66889 17.3561C1.39433 16.6017 1.43064 15.7692 1.76985 15.0416L7.6878 2.35156" stroke="white" stroke-linecap="round"/>
+                            <path d="M3.23565 19.0641C4.75083 19.7706 6.55188 19.1151 7.25842 17.5999C7.96496 16.0847 7.30943 14.2837 5.79425 13.5772C4.27908 12.8706 2.47802 13.5261 1.77148 15.0413C1.06494 16.5565 1.72047 18.3576 3.23565 19.0641Z" stroke="white"/>
+                            </svg></i></li>
+                        <li><i class="orbit-icon"><svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M19.2837 15.042L13.3137 2.23898C12.9744 1.51135 12.3599 0.948314 11.6055 0.673741C10.851 0.399169 10.0184 0.43555 9.29074 0.774881C8.56311 1.11421 8.00007 1.7287 7.7255 2.48315C7.45093 3.23761 7.48731 4.07024 7.82664 4.79788L13.7971 17.6004C14.1363 18.3281 14.7507 18.8911 15.5052 19.1657C16.2596 19.4404 17.0922 19.4041 17.8198 19.0648C18.5474 18.7255 19.1105 18.1111 19.3851 17.3567C19.6597 16.6023 19.6229 15.7697 19.2837 15.042Z" stroke="white"/>
+                            <path d="M10.2427 11.1987L7.25695 17.6C7.08894 17.9603 6.85161 18.284 6.55851 18.5526C6.2654 18.8211 5.92227 19.0293 5.54869 19.1653C5.17512 19.3013 4.77842 19.3623 4.38126 19.3449C3.98409 19.3276 3.59423 19.2321 3.23395 19.0641C2.50642 18.7248 1.94346 18.1104 1.66889 17.3561C1.39433 16.6017 1.43064 15.7692 1.76985 15.0416L7.6878 2.35156" stroke="white" stroke-linecap="round"/>
+                            <path d="M3.23565 19.0641C4.75083 19.7706 6.55188 19.1151 7.25842 17.5999C7.96496 16.0847 7.30943 14.2837 5.79425 13.5772C4.27908 12.8706 2.47802 13.5261 1.77148 15.0413C1.06494 16.5565 1.72047 18.3576 3.23565 19.0641Z" stroke="white"/>
+                            </svg></i></li>
+                        <li><i class="orbit-icon"><svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M19.2837 15.042L13.3137 2.23898C12.9744 1.51135 12.3599 0.948314 11.6055 0.673741C10.851 0.399169 10.0184 0.43555 9.29074 0.774881C8.56311 1.11421 8.00007 1.7287 7.7255 2.48315C7.45093 3.23761 7.48731 4.07024 7.82664 4.79788L13.7971 17.6004C14.1363 18.3281 14.7507 18.8911 15.5052 19.1657C16.2596 19.4404 17.0922 19.4041 17.8198 19.0648C18.5474 18.7255 19.1105 18.1111 19.3851 17.3567C19.6597 16.6023 19.6229 15.7697 19.2837 15.042Z" stroke="white"/>
+                            <path d="M10.2427 11.1987L7.25695 17.6C7.08894 17.9603 6.85161 18.284 6.55851 18.5526C6.2654 18.8211 5.92227 19.0293 5.54869 19.1653C5.17512 19.3013 4.77842 19.3623 4.38126 19.3449C3.98409 19.3276 3.59423 19.2321 3.23395 19.0641C2.50642 18.7248 1.94346 18.1104 1.66889 17.3561C1.39433 16.6017 1.43064 15.7692 1.76985 15.0416L7.6878 2.35156" stroke="white" stroke-linecap="round"/>
+                            <path d="M3.23565 19.0641C4.75083 19.7706 6.55188 19.1151 7.25842 17.5999C7.96496 16.0847 7.30943 14.2837 5.79425 13.5772C4.27908 12.8706 2.47802 13.5261 1.77148 15.0413C1.06494 16.5565 1.72047 18.3576 3.23565 19.0641Z" stroke="white"/>
+                            </svg></i></li>
+                        <li><i class="orbit-icon"><svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M19.2837 15.042L13.3137 2.23898C12.9744 1.51135 12.3599 0.948314 11.6055 0.673741C10.851 0.399169 10.0184 0.43555 9.29074 0.774881C8.56311 1.11421 8.00007 1.7287 7.7255 2.48315C7.45093 3.23761 7.48731 4.07024 7.82664 4.79788L13.7971 17.6004C14.1363 18.3281 14.7507 18.8911 15.5052 19.1657C16.2596 19.4404 17.0922 19.4041 17.8198 19.0648C18.5474 18.7255 19.1105 18.1111 19.3851 17.3567C19.6597 16.6023 19.6229 15.7697 19.2837 15.042Z" stroke="white"/>
+                            <path d="M10.2427 11.1987L7.25695 17.6C7.08894 17.9603 6.85161 18.284 6.55851 18.5526C6.2654 18.8211 5.92227 19.0293 5.54869 19.1653C5.17512 19.3013 4.77842 19.3623 4.38126 19.3449C3.98409 19.3276 3.59423 19.2321 3.23395 19.0641C2.50642 18.7248 1.94346 18.1104 1.66889 17.3561C1.39433 16.6017 1.43064 15.7692 1.76985 15.0416L7.6878 2.35156" stroke="white" stroke-linecap="round"/>
+                            <path d="M3.23565 19.0641C4.75083 19.7706 6.55188 19.1151 7.25842 17.5999C7.96496 16.0847 7.30943 14.2837 5.79425 13.5772C4.27908 12.8706 2.47802 13.5261 1.77148 15.0413C1.06494 16.5565 1.72047 18.3576 3.23565 19.0641Z" stroke="white"/>
+                            </svg></i></li>
+                        <li><i class="orbit-icon"><svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M19.2837 15.042L13.3137 2.23898C12.9744 1.51135 12.3599 0.948314 11.6055 0.673741C10.851 0.399169 10.0184 0.43555 9.29074 0.774881C8.56311 1.11421 8.00007 1.7287 7.7255 2.48315C7.45093 3.23761 7.48731 4.07024 7.82664 4.79788L13.7971 17.6004C14.1363 18.3281 14.7507 18.8911 15.5052 19.1657C16.2596 19.4404 17.0922 19.4041 17.8198 19.0648C18.5474 18.7255 19.1105 18.1111 19.3851 17.3567C19.6597 16.6023 19.6229 15.7697 19.2837 15.042Z" stroke="white"/>
+                            <path d="M10.2427 11.1987L7.25695 17.6C7.08894 17.9603 6.85161 18.284 6.55851 18.5526C6.2654 18.8211 5.92227 19.0293 5.54869 19.1653C5.17512 19.3013 4.77842 19.3623 4.38126 19.3449C3.98409 19.3276 3.59423 19.2321 3.23395 19.0641C2.50642 18.7248 1.94346 18.1104 1.66889 17.3561C1.39433 16.6017 1.43064 15.7692 1.76985 15.0416L7.6878 2.35156" stroke="white" stroke-linecap="round"/>
+                            <path d="M3.23565 19.0641C4.75083 19.7706 6.55188 19.1151 7.25842 17.5999C7.96496 16.0847 7.30943 14.2837 5.79425 13.5772C4.27908 12.8706 2.47802 13.5261 1.77148 15.0413C1.06494 16.5565 1.72047 18.3576 3.23565 19.0641Z" stroke="white"/>
+                            </svg></i></li>
+                        <li><i class="orbit-icon"><svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M19.2837 15.042L13.3137 2.23898C12.9744 1.51135 12.3599 0.948314 11.6055 0.673741C10.851 0.399169 10.0184 0.43555 9.29074 0.774881C8.56311 1.11421 8.00007 1.7287 7.7255 2.48315C7.45093 3.23761 7.48731 4.07024 7.82664 4.79788L13.7971 17.6004C14.1363 18.3281 14.7507 18.8911 15.5052 19.1657C16.2596 19.4404 17.0922 19.4041 17.8198 19.0648C18.5474 18.7255 19.1105 18.1111 19.3851 17.3567C19.6597 16.6023 19.6229 15.7697 19.2837 15.042Z" stroke="white"/>
+                            <path d="M10.2427 11.1987L7.25695 17.6C7.08894 17.9603 6.85161 18.284 6.55851 18.5526C6.2654 18.8211 5.92227 19.0293 5.54869 19.1653C5.17512 19.3013 4.77842 19.3623 4.38126 19.3449C3.98409 19.3276 3.59423 19.2321 3.23395 19.0641C2.50642 18.7248 1.94346 18.1104 1.66889 17.3561C1.39433 16.6017 1.43064 15.7692 1.76985 15.0416L7.6878 2.35156" stroke="white" stroke-linecap="round"/>
+                            <path d="M3.23565 19.0641C4.75083 19.7706 6.55188 19.1151 7.25842 17.5999C7.96496 16.0847 7.30943 14.2837 5.79425 13.5772C4.27908 12.8706 2.47802 13.5261 1.77148 15.0413C1.06494 16.5565 1.72047 18.3576 3.23565 19.0641Z" stroke="white"/>
+                            </svg></i></li>
+                        <li><i class="orbit-icon"><svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M19.2837 15.042L13.3137 2.23898C12.9744 1.51135 12.3599 0.948314 11.6055 0.673741C10.851 0.399169 10.0184 0.43555 9.29074 0.774881C8.56311 1.11421 8.00007 1.7287 7.7255 2.48315C7.45093 3.23761 7.48731 4.07024 7.82664 4.79788L13.7971 17.6004C14.1363 18.3281 14.7507 18.8911 15.5052 19.1657C16.2596 19.4404 17.0922 19.4041 17.8198 19.0648C18.5474 18.7255 19.1105 18.1111 19.3851 17.3567C19.6597 16.6023 19.6229 15.7697 19.2837 15.042Z" stroke="white"/>
+                            <path d="M10.2427 11.1987L7.25695 17.6C7.08894 17.9603 6.85161 18.284 6.55851 18.5526C6.2654 18.8211 5.92227 19.0293 5.54869 19.1653C5.17512 19.3013 4.77842 19.3623 4.38126 19.3449C3.98409 19.3276 3.59423 19.2321 3.23395 19.0641C2.50642 18.7248 1.94346 18.1104 1.66889 17.3561C1.39433 16.6017 1.43064 15.7692 1.76985 15.0416L7.6878 2.35156" stroke="white" stroke-linecap="round"/>
+                            <path d="M3.23565 19.0641C4.75083 19.7706 6.55188 19.1151 7.25842 17.5999C7.96496 16.0847 7.30943 14.2837 5.79425 13.5772C4.27908 12.8706 2.47802 13.5261 1.77148 15.0413C1.06494 16.5565 1.72047 18.3576 3.23565 19.0641Z" stroke="white"/>
+                            </svg></i></li>
+                        <li><i class="orbit-icon"><svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M19.2837 15.042L13.3137 2.23898C12.9744 1.51135 12.3599 0.948314 11.6055 0.673741C10.851 0.399169 10.0184 0.43555 9.29074 0.774881C8.56311 1.11421 8.00007 1.7287 7.7255 2.48315C7.45093 3.23761 7.48731 4.07024 7.82664 4.79788L13.7971 17.6004C14.1363 18.3281 14.7507 18.8911 15.5052 19.1657C16.2596 19.4404 17.0922 19.4041 17.8198 19.0648C18.5474 18.7255 19.1105 18.1111 19.3851 17.3567C19.6597 16.6023 19.6229 15.7697 19.2837 15.042Z" stroke="white"/>
+                            <path d="M10.2427 11.1987L7.25695 17.6C7.08894 17.9603 6.85161 18.284 6.55851 18.5526C6.2654 18.8211 5.92227 19.0293 5.54869 19.1653C5.17512 19.3013 4.77842 19.3623 4.38126 19.3449C3.98409 19.3276 3.59423 19.2321 3.23395 19.0641C2.50642 18.7248 1.94346 18.1104 1.66889 17.3561C1.39433 16.6017 1.43064 15.7692 1.76985 15.0416L7.6878 2.35156" stroke="white" stroke-linecap="round"/>
+                            <path d="M3.23565 19.0641C4.75083 19.7706 6.55188 19.1151 7.25842 17.5999C7.96496 16.0847 7.30943 14.2837 5.79425 13.5772C4.27908 12.8706 2.47802 13.5261 1.77148 15.0413C1.06494 16.5565 1.72047 18.3576 3.23565 19.0641Z" stroke="white"/>
+                            </svg></i></li>
+                    </ul>
+                    </li>
+                    <div className=' w-full h-full rounded-full border-solid border-2 border-[#3FBAFF] p-8 relative'>
+                    
+                        <div className=' w-full h-full rounded-full border-solid border-2 border-[#3FBAFF] p-4'>
+                            <div className=' w-full h-full rounded-full bg-gradient-to-b from-[#16AAC8]/50 to-[#1098CB]/50 p-10'>
+                                <div className=' w-full h-full rounded-full bg-gradient-to-b from-[#16AAC8]/80 to-[#1098CB]/80 p-10 shadow-[0_35px_35px_-15px_rgba(0,0,0,0.3),0_35px_35px_55px_rgba(0,0,0,0.3)]'>
+                                    <div className=' w-full h-full flex justify-center items-center rounded-full bg-gradient-to-b from-[#1098CB] to-[#00D4FF] shadow-[0_35px_35px_-15px_rgba(0,0,0,0.3),0_35px_35px_55px_rgba(0,0,0,0.3)]'>
+                                        <svg width="188" height="134" viewBox="0 0 188 134" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M56.9182 133.948L77.9112 100.176H106.184C112.899 100.176 120.798 100.176 126.17 100.568C123.148 95.8586 118.43 87.8027 114.737 81.5233L94.5924 46.7593L41.792 133.948H0.654297L72.7159 15.135C77.4164 7.49224 84.308 0.407227 95.2286 0.407227C105.637 0.407227 112.528 6.89322 117.388 15.135L187.612 133.948H56.9182Z" fill="#F0FAFD" fill-opacity="0.7"/>
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </ul>
+            </div>
+
+        </div>
+
+    </div>
 
 
 
